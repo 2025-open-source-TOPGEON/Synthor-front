77c46cafdcba2a73e447dcd2760dace4bbe1f62e
vvineey <vvin506@naver.com>
2025-08-03 08:44:50 +0900

    fix : 새로운 필드 추가 시 마지막 값 복사되도록 수정

--- src/features/synthorPage/hooks/useFieldList.js
+++ src/features/synthorPage/hooks/useFieldList.js
@@ -3,8 +3,8 @@ import { useState } from "react";
 export default function useFieldList() {
     const [fields, setFields] = useState([
         { id: "1", fieldName: "full_name", fieldType: "Full Name", constraint: "" },
-        { id: "1", fieldName: "gender", fieldType: "Gender", constraint: "" },
-        { id: "1", fieldName: "age", fieldType: "Number", constraint: "" },
+        { id: "2", fieldName: "gender", fieldType: "Gender", constraint: "" },
+        { id: "3", fieldName: "age", fieldType: "Number", constraint: "" },
     ]);
 
     const handleChange = (id, key, value) => {
@@ -18,16 +18,25 @@ export default function useFieldList() {
     };
 
     const handleAdd = () => {
-        setFields((prev) => [
-            ...prev,
-            {
-                id: Date.now().toString(),
+        setFields((prev) => {
+            const last = prev[prev.length - 1] || {
                 fieldName: "",
-                fieldType: "Full Name",
+                fieldType: "",
                 constraint: "",
-            },
-        ]);
+            };
+
+            return [
+                ...prev,
+                {
+                    id: last.id + 1,
+                    fieldName: last.fieldName,
+                    fieldType: last.fieldType,
+                    constraint: last.constraint,
+                },
+            ];
+        });
     };
+
 
     const reorderFields = (activeId, overId) => {
         setFields((prev) => {
