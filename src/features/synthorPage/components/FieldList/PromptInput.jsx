8c0bcb6aa8baff4c7fdbed732a3dd5f8ebb1a171
vvineey <vvin506@naver.com>
2025-08-15 16:27:54 +0900

    style : 프롬프트 헤더 변경

--- src/features/synthorPage/components/FieldList/PromptInput.jsx
+++ src/features/synthorPage/components/FieldList/PromptInput.jsx
@@ -15,7 +15,7 @@ export default function PromptInput({ id, prompt, onChange, selectedType }) {
 
     return (
         <div className="flex-1">
-            <p className="text-s text-gray-200 mb-2">Prompt</p>
+            <p className="text-s text-gray-200 mb-2">Constraints Prompt</p>
             <div className="flex-1 border border-gray-700 focus-within:border-[#8E25E2] rounded-[15px] p-2 bg-transparent">
                 <InputBox
                     value={prompt}
